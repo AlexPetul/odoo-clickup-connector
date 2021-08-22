7c7d25f2430776cf2fb0172fbc7f8923f38e20bf
Petul Alexey <a.petul@icode.by>
2021-08-22 17:41:40 +0300

    Retrieve database name from config

--- clickup_connector/clickup/decorators.py
+++ clickup_connector/clickup/decorators.py
@@ -1,13 +1,17 @@
 from functools import wraps
 
 from odoo import api, SUPERUSER_ID
+from odoo.tools import config
 from odoo.modules.registry import Registry
 
 
 def api_method(func):
     @wraps(func)
     def closure(*args, **kwargs):
-        db_registry = Registry.new(db_name=args[2])
+        try:
+            db_registry = Registry.new(db_name=args[2])
+        except (IndexError, Exception):
+            db_registry = Registry.new(db_name=config["db_name"])
         with api.Environment.manage(), db_registry.cursor() as cr:
             args[0].env = api.Environment(cr=cr, uid=SUPERUSER_ID, context={})
             return func(*args, **kwargs)
